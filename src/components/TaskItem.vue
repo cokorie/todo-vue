e9e6e7ed540c962508bb86898d08f8cddc7fb110
cokorie <chijioke.okorie@gmail.com>
2026-09-13 22:23:13 -0500

    add completed item strikethrough

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -1,6 +1,6 @@
 <template>
     <li>
-        <button><i class="far fa-circle"></i>{{task.title}}</button>
+        <button v-bind:class="className"><i class="far fa-circle"></i>{{task.title}}</button>
         <button @click="$emit('remove')"><i class="far fa-trash-alt"></i></button>
     </li>
 </template>
@@ -9,5 +9,14 @@
 export default {
     name: 'TaskItem',
     props: ['task'],
+    computed: {
+        className() {
+            let classes = ['toggle'];
+            if(this.task.completed) {
+                classes.push('toggle-completed');
+            }
+            return classes;
+        }
+    }
 }
 </script>
