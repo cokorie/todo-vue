7c1d7fddd87f311317de5aea7575aa0c739d0568
cokorie <chijioke.okorie@gmail.com>
2026-09-13 20:41:22 -0500

    add each task item

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -14,7 +14,7 @@
       <div class="taskItems">
         <ul>
           <li v-for="task in tasks" :key="task.id">
-            <button>Walk Dog</button>
+            <button>{{task.title}}</button>
             <button><i class="far fa-trash-alt"></i></button>
           </li>
         </ul>
