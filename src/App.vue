69d7dd2204a229da6abaf91d21253b84b433b97b
cokorie <chijioke.okorie@gmail.com>
2026-09-13 20:26:45 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <task></task>
+  </div>
+</template>
+
+<script>
+import Task from "./components/Task";
+
+export default {
+  name: "App",
+  components: {
+    Task,
+  },
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: "Walk Dog",
+          completed: true,
+        },
+        {
+          id: 2,
+          title: "Read Book",
+          completed: true,
+        },
+        {
+          id: 3,
+          title: "Grocery Shopping",
+          completed: false,
+        },
+        {
+          id: 4,
+          title: "Water Lawn",
+          completed: false,
+        },
+        {
+          id: 5,
+          title: "Call Friend",
+          completed: false,
+        },
+      ],
+    };
+  },
+};
+</script>
