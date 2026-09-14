41dcf99df4b6bc35460969be7fd234ea0dbc57da
cokorie <chijioke.okorie@gmail.com>
2026-09-13 20:32:18 -0500

    add initial list item

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -21,6 +21,10 @@
             <button>Read Book</button>
             <button><i class="far fa-trash-alt"></i></button>
           </li>
+          <li>
+            <button>Grocery Shopping</button>
+            <button><i class="far fa-trash-alt"></i></button>
+          </li>
         </ul>
       </div>
       <!-- buttons -->
